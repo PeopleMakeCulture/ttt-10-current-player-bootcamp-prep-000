21cfcc7ef3abdf8f52bbbcf92e4726e8b2198f46
Jing Cao <jingcao.yale@gmail.com>
2018-07-14 20:59:17 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -10,9 +10,5 @@ end
 
 def current_player(board)
   #If the turn count is an even number, the #current_player method should return "X", otherwise, it should return "O".
-  if turn_count(board) % 2 == 0
-    return "X"
-  else
-    return "O"
-  end
+  turn_count(board) % 2 == 0? return "X": return "O"
 end
