415180012aa1daeaeb920097a274dccba658dffb
Jing Cao <jingcao.yale@gmail.com>
2018-07-14 21:02:06 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,10 +1,9 @@
 def turn_count(board)
   counter = 0
-  board.each do |box|
+  board.each {|box|
     if box == "X" || box == "O"
       counter += 1
-    end
-  end
+    end}
   return counter
 end
 
